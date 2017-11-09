d3088feedb3ba7f0b12236479bb43528e1099eb7
rogerman <rogerman@users.noreply.github.com>
2017-11-08 21:04:57 -0800

    Add a dummy test module. Hopefully Travis-CI works this time...

---
+++ desmume/src/frontend/cocoa/DeSmuME-Test/DeSmuME_Test.m
@@ -0,0 +1,48 @@
+/*
+	Copyright (C) 2017 DeSmuME team
+ 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import <XCTest/XCTest.h>
+
+@interface DeSmuME_Test : XCTestCase
+
+@end
+
+@implementation DeSmuME_Test
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
